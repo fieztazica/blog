9d69a2da2a2d011640550cbb5aa4636957dc4fb5
dat09t <dat09t@gmail.com>
2024-10-30 08:29:44 +0000

    update greeting words

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import React from 'react'
-import CustomLink from './Link'
-import { Transition } from '@headlessui/react'
-import { cn } from '@/lib/utils'
-import { ChevronDown } from 'lucide-react'
-import { useGreeting } from '@/lib/hooks/useGreeting'
+import { greetings } from '@/data/langs/greetings'
 import { useIsScrollTop } from '@/lib/hooks/useIsScrollTop'
-import Image, { getSrcUrl } from './Image'
+import { cn } from '@/lib/utils'
+import { Transition } from '@headlessui/react'
+import { ChevronDown } from 'lucide-react'
+import React from 'react'
+import Image from './Image'
+import CustomLink from './Link'
+import { FlipWords } from './ui/flip-words'
 
 function Hero() {
-  const { greeting, isShowing } = useGreeting()
   const isTop = useIsScrollTop()
   const readMoreRef = React.useRef<HTMLButtonElement>(null)
 
@@ -22,19 +22,17 @@ function Hero() {
         <CustomLink href="/about">
           <div
             className={
-              'border-mine-shaft-200 dark:border-mine-shaft-700 flex w-full justify-center rounded-lg border-2 bg-cover bg-center bg-no-repeat px-16 py-24'
+              'relative border-mine-shaft-200 dark:border-mine-shaft-700 items-center flex flex-col w-full justify-center rounded-lg border-2 px-16 py-24'
             }
-            style={{
-              backgroundImage: `url('${getSrcUrl('/static/images/bg.gif')}')`,
-            }}
           >
+            <img className="h-full w-full absolute inset-0 object-cover rounded-lg" src="/static/images/bg.gif"/>
             <Image
               src="/static/images/bumping.gif"
               alt="Fiezt bumping gif"
               width={1280}
               height={720}
               sizes="33vw"
-              className="h-full w-64 max-w-64"
+              className="absolute w-64 max-w-64"
             />
           </div>
         </CustomLink>
@@ -46,20 +44,8 @@ function Hero() {
               'mb-2 text-3xl font-extrabold leading-9 tracking-tight text-zinc-900 dark:text-zinc-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'
             )}
           >
-            <span>👋 </span>
-            <Transition show={isShowing} as={React.Fragment}>
-              <Transition.Child
-                as={React.Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="scale-0 opacity-0"
-                enterTo="scale-100 opacity-95"
-                leave="transition ease-in duration-200 transform"
-                leaveFrom="scale-100 opacity-95"
-                leaveTo="scale-0 opacity-0"
-              >
-                <span>{greeting}!</span>
-              </Transition.Child>
-            </Transition>
+            <span>👋</span>
+            <FlipWords words={greetings.map(word => word + "!")} />
           </h1>
 
           <p className="mb-4 text-lg text-zinc-500 dark:text-zinc-400">
